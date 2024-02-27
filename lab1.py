58c9589dd3abd2de5dde8f3136354841044bcdca
Kassiani Karakosta <75204831+kassianikarakosta@users.noreply.github.com>
2024-02-27 11:38:56 +0200

    Print Headers

--- lab1.py
+++ lab1.py
@@ -5,3 +5,13 @@ url = input("Give a URL:\n")  # προσδιορισμός του url
 if not url.startswith("https://"):
     url = "https://" + url
 
+print("\n")
+
+with requests.get(url) as response:  # το αντικείμενο response
+
+    # Εκτυπώνουμε τις κεφαλίδες της απόκρισης HTTP
+    print("Κεφαλίδες της απόκρισης HTTP:")
+    for header, value in response.headers.items():
+        print(f"{header}: {value}")
+
+    print("\n")
